01c7a4c0fccff28e0f2f0a07118aa07ac3c42910
AlekseyPriz <aleksey.priz@gmail.com>
2022-02-01 12:47:30 +0300

    [main] Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as math from 'mathjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'changeDetection';
+
+  constructor() {
+    this.addNumber(10);
+  }
+
+  private renderCounter: number = 0;
+
+  public arr: Array<number> = [];
+
+  public refresh(i: number): void {
+    this.arr[i] = this.getRandomFactorial();
+  }
+
+  public action() {
+    this.renderCounter += 1;
+    let random = Math.floor(Math.random() * 100);
+    console.log(`${this.renderCounter}) factorial(${random}) => ${this.factorial(random)}`);
+  }
+
+  public addNumber(num: number): void {
+    for (let i = 0; i < num; i++) {
+      this.arr.push(this.getRandomFactorial());
+    }
+  }
+
+  private getRandomFactorial(): number {
+    let random = Math.floor(Math.random() * 100);
+    return this.factorial(random);
+  }
+
+  public factorial(n: number): number {
+    return math.factorial(n);
+  }
+
 }
